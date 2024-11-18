0614200250088914036ab033aee51a985991ea5a
xenotation <63644334+xenotation@users.noreply.github.com>
2024-11-18 20:54:43 +0200

    build

---
+++ data.js
@@ -0,0 +1,6 @@
+const webringData = [
+  { name: "Rain Marks", url: "https://rainmarks.com/", title: "Rain Marks Website" },
+  { name: "Night Beaches", url: "", title: "Night Beaches Project" },
+  { name: "░░░░░░░░░░", url: "", title: "Placeholder 1" },
+  { name: "░░░░░░░░░░", url: "", title: "Placeholder 2" }
+];
